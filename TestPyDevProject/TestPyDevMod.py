1f5bae62c3fc39adb1f191fb2fb9a46bd705a835
Noel Wijesinha <noelw@sciteqsoft.net>
2017-10-29 20:27:52 -0400

    updated

--- TestPyDevProject/TestPyDevMod.py
+++ TestPyDevProject/TestPyDevMod.py
@@ -6,6 +6,8 @@ Created on Oct 14, 2017
 
 @author: noel.wijesinha
 '''
+from array import array
+from builtins import str
 
 if __name__ == '__main__':
     pass
@@ -15,14 +17,21 @@ if __name__ == '__main__':
 # age = input("Your age, please? ")
 # print("So, you"+"'"+"re " + age + " years old, " + name + "!")
 
+'''
 matrix = [[1,2,3,4],
           [11,12,13,14],
           [21,22,23,24]]
-
+'''
+'''
 for i in range(0,3):
     for j in range(0,4):
         
         print(matrix[i][j])
+ '''
+  
+help(str)
+
+
         
 
     
