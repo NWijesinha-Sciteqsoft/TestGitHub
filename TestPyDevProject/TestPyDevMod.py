7eb77546fa38671473f7b3ebf71d12aa52600719
Noel Wijesinha <noelw@sciteqsoft.net>
2017-11-02 17:08:30 -0400

    Update 02-Nov-17 @ 5:08pm

--- TestPyDevProject/TestPyDevMod.py
+++ TestPyDevProject/TestPyDevMod.py
@@ -87,6 +87,89 @@ print(np_ef.shape)
 print(np_ef.dtype)
 '''
 
+'''
+# np array with zeros
+np_azero = np.zeros((3,4))
+print(type(np_azero))
+print(np_azero)
+print(np_azero.size)
+print(np_azero.shape)
+print(np_azero.dtype)
+'''
+'''
+#np array with ones
+np_aones = np.ones((2,3,4), dtype = np.int64)
+print(type(np_aones))
+print(np_aones)
+print(np_aones.size)
+print(np_aones.shape)
+print(np_aones.dtype)
+'''   
+'''
+# np empty array
+np_aempty = np.empty((2,3,4), dtype = np.int64)
+print(type(np_aempty))
+print(np_aempty)
+print(np_aempty.size)
+print(np_aempty.shape)
+print(np_aempty.dtype)
+'''
+'''
+np_array = np.random.random((6,4))
+#print(type(np_array))
+#print(np_array)
+#print(np_array.size)
+#print(np_array.shape)
+#print(np_array.dtype)
+#print(np_array.sum())
+#print(np_array.min())
+#print(np_array.max())
+#print(np_array.mean())
+#print(np_array[2,3])
+#print(np_array[:,1])
+#print(np_array[1:3,:])
+#print(np_array[1,2:4])
+#print(np_array[1:4,1:4])
 
+for row in np_array:
+    print(row)
 
-    
+for element in np_array.flat: 
+    print(element)
+print(np_array.ravel())
+print("")
+print("----------")
+print("")
+print(np_array.reshape(12,2))
+print("")
+print("----------")
+print("")
+print(np_array.T)
+print("")
+print("----------")
+print("")
+print(np_array.T.shape)
+print("")
+print("----------")
+print("")
+'''
+
+import matplotlib.pyplot as plt
+def mandelbrot( h,w, maxit=20 ):
+     """Returns an image of the Mandelbrot fractal of size (h,w)."""
+     y,x = np.ogrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]
+     c = x+y*1j
+     z = c
+     divtime = maxit + np.zeros(z.shape, dtype=int)
+
+     for i in range(maxit):
+         z = z**2 + c
+         diverge = z*np.conj(z) > 2**2            # who is diverging
+         div_now = diverge & (divtime==maxit)  # who is diverging now
+         divtime[div_now] = i                  # note when
+         z[diverge] = 2                        # avoid diverging too much
+
+     return divtime
+ 
+plt.imshow(mandelbrot(400,400))
+plt.show()
