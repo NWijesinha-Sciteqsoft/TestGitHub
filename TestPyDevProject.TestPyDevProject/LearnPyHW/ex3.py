4f66d98b1fca268fbff8c5cd2f68e2b62f7df4de
Noel Wijesinha <noelw@sciteqsoft.net>
2018-10-16 15:24:21 -0400

    latest Updates as of 16-Oct-18

---
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex3.py
@@ -0,0 +1,39 @@
+'''
+Created on Oct 11, 2018
+
+@author: nwijesinha
+'''
+# Print the description of first calculation
+print("I will now count my chickens:")
+
+# Calculate the hens
+print("Hens", float(25+30/6))
+#Calculate the Roosters
+print("Roosters", float(100-25*3%4))
+
+# Print the description of second calculation
+print('Now I will count the eggs:')
+
+# Print the number of eggs
+print(float(3+2+1-5+4%2-1/4+6))
+
+# Print statement to check if 3+2 less than 5-7
+print("Is it true that 3+2 < 5-7?")
+# Print the calculation to check if 3+2 less than 5-7
+print(float(3+2) < float(5-7))
+
+# Print the answer for 3+2
+print("What is 3+2?", float(3+2))
+
+# Print the answer for 5-7
+print("What is 5-7", float(5-7))
+
+# Print the statement to check if a calculation is false
+print("Oh, that's why it's False")
+
+# Print to check if 5 greater than 2
+print("Is it greater?", 5>2)
+# Print to check if 5 greater than or equal to 2
+print("Is it greater or equal", 5 >= -2)
+# Print to check if 5 less than or equal to 2
+print("Is it less or equal", 5<= -2)
