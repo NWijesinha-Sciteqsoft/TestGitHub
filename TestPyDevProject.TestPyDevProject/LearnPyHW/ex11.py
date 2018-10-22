0b45685c310b406c31464ac28b9dc2194c7932a5
Noel Wijesinha <noelw@sciteqsoft.net>
2018-10-22 17:17:35 -0400

    updated with latest

--- TestPyDevProject.TestPyDevProject/LearnPyHW/ex11.py
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex11.py
@@ -9,12 +9,16 @@ This example is to understand how to receive input from user using Python code
 from click._compat import raw_input
 
 if __name__ == '__main__':
+    # Ask the user for age and save in age variable
     print("How old are you?")
     age = raw_input()
+    # Ask the user for the height and save in height variable
     print("How tall are you?")
     height = raw_input()
+    # Ask the user for weight and save in weight variable
     print("How much do you weigh?")
     weight = raw_input()
 
-print("So you're %r years old, %r cm tall and %r lbs heavy" % (age, height, weight))
+    # Print the age, height and weight of the user
+    print("So you're %r years old, %r cm tall and %r lbs heavy" % (age, height, weight))
 
---
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex12.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 22, 2018
+
+@author: nwijesinha
+'''
+from click._compat import *
+
+if __name__ == '__main__':
+    # Ask the user for age and save in age variable
+    age = raw_input("How old are you? ")
+    # Ask the user for the height and save in height variable
+    height = raw_input("How tall are you? ")
+    # Ask the user for weight and save in weight variable
+    weight = raw_input("How much do you weigh? ")
+    # Print the age, height and weight of the user
+    print("So you're %r years old, %r cm tall and %r lbs heavy" % (age, height, weight))
