e9d5aee7bf1e65d12a3fbb410648621ecdfe82ab
Noel Wijesinha <noelw@sciteqsoft.net>
2018-10-22 13:41:40 -0400

    Upadted with latest as of 22-Oct-18 1:41

--- TestPyDevProject.TestPyDevProject/LearnPyHW/ex9.py
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex9.py
@@ -3,3 +3,21 @@ Created on Oct 22, 2018
 
 @author: nwijesinha
 '''
+# Here's some new strange stuff, remember type it exactly
+
+# Assign days of the week in word three letter abbreviation form to days variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assign three letter month abbreviation to months variable with a new line after each month from January to August
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# Print the days
+print("Here are the days: ", days)
+# Print the months
+print('Here are the months: ', months)
+
+# Print multiple line using """ in the print statement
+print("""
+There is something going on here,
+With the three double-quotes.
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5, or 6.
+""")
