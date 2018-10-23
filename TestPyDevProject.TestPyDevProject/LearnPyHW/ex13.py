94c6ca4d8cd49728a5aa57cd7c128971f13f44a3
Noel Wijesinha <noelw@sciteqsoft.net>
2018-10-23 16:58:10 -0400

    Updated with latest

---
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex13.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 23, 2018
+
+@author: nwijesinha
+
+This example is using alternative input method to pass a variable
+
+'''
+
+from sys import argv
+    
+# Create variable for the example
+script, first, second, third = argv
+
+# using print method to assign information to variables
+print("This script is called: ", script)
+print("Your first variable is: ", first)
+print("Your first variable is: ", second)
+print("Your first variable is: ", third)
