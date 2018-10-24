2837ea31d1a9b876a098cf5cd677bc350afe1d5c
Noel Wijesinha <noelw@sciteqsoft.net>
2018-10-24 17:28:24 -0400

    Updated with the latest

--- TestPyDevProject.TestPyDevProject/LearnPyHW/ex14.py
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex14.py
@@ -6,13 +6,31 @@ Created on Oct 24, 2018
 
 # Import Sys.argument library
 from sys import argv
+from click._compat import raw_input
 
 # assign arguments when script is run to script, username
-script, username = argv
+script, user_name = argv
 # Create variable prompt
 prompt = '>'
 
 # Print the arguments collected and prompt the user for input
-print("Hi %s, I'm the %s script.", % (username, script))
+print("Hi %s, I'm the %s script." % (user_name, script))
 print("I'd like to ask you a few questions.")
-print("Do you like me %s", % username)
+print("Do you like me %s" % user_name)
+likes = raw_input(prompt)
+
+# Accept second input from user
+print("Where do you live %s?" % user_name)
+lives = raw_input(prompt)
+
+# Accept third input from user
+print("What kind of computer do you have?")
+computer = raw_input(prompt)
+
+# Prin the user input on the screen
+print("""
+Alright, so you have said %r about liking me
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice
+
+""" % (likes, lives, computer))
