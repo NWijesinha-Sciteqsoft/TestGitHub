2e09b079479b0be5a1dddc7cc47fbba9062f60fa
Noel Wijesinha <noelw@sciteqsoft.net>
2018-11-19 12:43:05 -0500

    Updated with the latest Learn Python HardWay Exercises
    Signed-off-by: Noel Wijesinha <noelw@sciteqsoft.net>

--- TestPyDevProject.TestPyDevProject/LearnPyHW/ex44c.py
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex44c.py
@@ -5,3 +5,18 @@ Created on Nov 15, 2018
 
 '''
 
+class Parent(object):
+    def altered(self):
+        print("PARENT altered()")
+        
+class Child(Parent):
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
---
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex44d.py
@@ -0,0 +1,46 @@
+'''
+Created on Nov 19, 2018
+
+@author: nwijesinha
+'''
+
+class Parent(object):
+    # Parent class
+    
+    def override(self):
+        # Override function which prints "PARENT override()"
+        print("PARENT override()")
+
+    def implicit(self):
+        # Implicit function which prints "PARENT implicit()"
+        print("PARENT implicit()")
+        
+    def altered(self):
+        # Altered function which prints "PARENT altered()"
+        print("PARENT altered()")
+        
+class Child(Parent):
+    # Child class
+    def override(self):
+        # Override function which override the parent override function and prints "CHILD override()"
+        print("CHILD override()")
+    
+    def altered(self):
+        # Altered function which over ride parents altered function prints "CHILD altered()"
+        print("CHILD, BEFORE PARENT altered()")
+        # Reset Parent altered function which prints "PARENT altered()"
+        super(Child, self).altered()
+        # Print the state of the altered function
+        print("CHILD, AFTER PARENT altered()")
+        
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
