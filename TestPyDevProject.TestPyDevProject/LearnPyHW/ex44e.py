f61326d66048375125b9add9f0a728b45510d612
NWijesinha-Sciteqsoft <noelw@sciteqsoft.net>
2018-11-19 15:34:14 -0500

    Updated with latest

--- TestPyDevProject.TestPyDevProject/LearnPyHW/ex44e.py
+++ TestPyDevProject.TestPyDevProject/LearnPyHW/ex44e.py
@@ -4,5 +4,36 @@ Created on Nov 19, 2018
 @author: nwijesinha
 '''
 
-class Parent(object):
-    pass
+class Other(object):
+    
+    def override(self):
+        print("OTHER override()")
+        
+    def implicit(self):
+        print("OTHER implicit()")
+        
+    def altered(self):
+        print("OTHER altered()")
+        
+class Child(object):
+    
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+    
+    def override(self):
+        print("CHILD override()")
+        
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+        
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
