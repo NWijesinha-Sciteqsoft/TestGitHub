cc6765a7ec51dab75e36a3a212584394b8edddc8
NWijesinha-Sciteqsoft <noelw@sciteqsoft.net>
2018-11-20 22:18:56 -0500

    Updated with latest

---
+++ TestPyDevProject.TestPyDevProject/LearnPyDS/ex4.py
@@ -0,0 +1,12 @@
+'''
+Created on Nov 20, 2018
+
+@author: nwijesinha
+
+This exercise to learn dictionary or dict in python
+Dictionary (Dict):
+In Dictionary each key is separated from its value by a colon (:), 
+the items are separated by commas, and the whole thing is enclosed in curly braces. 
+An empty dictionary without any items is written with just two curly braces, like this: {}.
+
+'''
